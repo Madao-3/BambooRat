f94684aa56b683640de8803641a79e3f7bbef30d
Madao <chrishyman256@gmail.com>
2018-10-07 17:29:54 +0800

    fix a typo

--- lib/bamboo_rat/diff.rb
+++ lib/bamboo_rat/diff.rb
@@ -14,7 +14,7 @@ module BambooRat
     def formatted_data
       return { diff: @diff_components }.to_json if @format == 'simple'
       return @diff_components[:js].join(',') if @format == 'js'
-      return @diff_components[:ruby].join(',' if @format == 'ruby'
+      return @diff_components[:ruby].join(',') if @format == 'ruby'
       {
         js_components: @tree.js_components.map(&:path),
         ruby_components: @tree.ruby_components.map(&:path),
