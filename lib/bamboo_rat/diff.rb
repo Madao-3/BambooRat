0e3816e2f04414502b86f4535cc6590c709b639e
Madao <chrishyman256@gmail.com>
2018-10-07 15:41:56 +0800

    add more option on diff

--- lib/bamboo_rat/diff.rb
+++ lib/bamboo_rat/diff.rb
@@ -13,6 +13,8 @@ module BambooRat
 
     def formatted_data
       return { diff: @diff_components }.to_json if @format == 'simple'
+      return @diff_components[:js].join(',') if @format == 'js'
+      return @diff_components[:ruby].join(',' if @format == 'ruby'
       {
         js_components: @tree.js_components.map(&:path),
         ruby_components: @tree.ruby_components.map(&:path),
